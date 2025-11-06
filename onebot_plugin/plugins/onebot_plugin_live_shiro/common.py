dc61abd63cadf13cf13ace48aa585ddef2482509
OtakuLuo <hl1282456555@outlook.com>
2025-11-06 22:24:01 +0800

    新增common配置化处理

--- onebot_plugin/plugins/onebot_plugin_live_shiro/common.py
+++ onebot_plugin/plugins/onebot_plugin_live_shiro/common.py
@@ -1,16 +1,26 @@
-from nonebot import on_command
+from nonebot import on_command, get_plugin_config
 from nonebot.rule import to_me
+
+from .config import Config
+
+plugin_config = get_plugin_config(Config)
 
 twitch_command = on_command("twitch", rule=to_me(), force_whitespace=True)
 
 @twitch_command.handle()
 async def _():
-    await twitch_command.finish("Shiro的Twitch频道是：https://www.twitch.tv/shiroyukitv")
+    await twitch_command.finish(f"Shiro的Twitch频道是：{plugin_config.live_shiro_twitch_url}")
 
 discord_command = on_command("discord", rule=to_me(), force_whitespace=True)
 
 @discord_command.handle()
 async def _():
-    await discord_command.finish("Shiro的Discord服务器是：https://discord.gg/uAYcmfszP")
+    await discord_command.finish(f"Shiro的Discord服务器是：{plugin_config.live_shiro_discord_url}")
+
+steam_command = on_command("steam", rule=to_me(), force_whitespace=True)
+
+@steam_command.handle()
+async def _():
+    await steam_command.finish(f"Shiro的Steam好友码是：{plugin_config.live_shiro_steam_friend_code}")
 
 __all__ = ["discord_command", "twitch_command"]
--- onebot_plugin/plugins/onebot_plugin_live_shiro/config.py
+++ onebot_plugin/plugins/onebot_plugin_live_shiro/config.py
@@ -7,4 +7,7 @@ class Config(BaseModel):
     live_shiro_group_ids: list[int] = []
     live_shiro_sleep_clock_hour: int = 1
     live_shiro_sleep_clock_minute: int = 0
+    live_shiro_twitch_url: str = ""
+    live_shiro_discord_url: str = ""
+    live_shiro_steam_friend_code: int = -1
 
