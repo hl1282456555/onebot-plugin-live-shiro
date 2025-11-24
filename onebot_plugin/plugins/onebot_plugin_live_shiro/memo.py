11864ec7dfaeda49a93251a42723e05b92bd908a
OtakuLuo <hl1282456555@outlook.com>
2025-11-24 21:50:11 +0800

    测试代码

--- onebot_plugin/plugins/onebot_plugin_live_shiro/memo.py
+++ onebot_plugin/plugins/onebot_plugin_live_shiro/memo.py
@@ -9,7 +9,7 @@ memo_command_group = CommandGroup("memo", rule=to_me())
 
 help_command = memo_command_group.command("help")
 @help_command.handle()
-async def _(bot, event: MessageEvent):
+async def handle_memo_help_command(bot, event: MessageEvent):
     await help_command.finish(Message([
         MessageSegment.reply(event.message_id),
         MessageSegment.text("定时备忘录帮助文档：\n"),
@@ -23,22 +23,22 @@ async def _(bot, event: MessageEvent):
 
 add_command = memo_command_group.command("add")
 @add_command.handle()
-async def _(bot, event: MessageEvent):
+async def handle_memo_add_command(bot, event: MessageEvent):
     pass
 
 list_command = memo_command_group.command("list")
 @list_command.handle()
-async def _(bot, event: MessageEvent):
+async def handle_memo_list_command(bot, event: MessageEvent):
     pass
 
 delete_command = memo_command_group.command("delete")
 @delete_command.handle()
-async def _(bot, event: MessageEvent):
+async def handle_memo_delete_command(bot, event: MessageEvent):
     pass
 
 edit_command = memo_command_group.command("edit")
 @edit_command.handle()
-async def _(bot, event: MessageEvent):
+async def handle_memo_edit_command(bot, event: MessageEvent):
     pass
 
 async def memo_bot_connect_handler(bot: Bot) -> Optional[Message]:
