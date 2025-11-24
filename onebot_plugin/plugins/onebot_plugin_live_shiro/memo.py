f3f1a6c5fae84124dcff2bcff458746f32518c03
OtakuLuo <hl1282456555@outlook.com>
2025-11-24 21:23:32 +0800

    修改memo触发条件

--- onebot_plugin/plugins/onebot_plugin_live_shiro/memo.py
+++ onebot_plugin/plugins/onebot_plugin_live_shiro/memo.py
@@ -3,14 +3,13 @@ from typing import Optional
 from nonebot import CommandGroup
 from nonebot.adapters import Bot
 from nonebot.adapters.onebot.v11 import Message, MessageEvent, MessageSegment
-from nonebot.params import ArgPlainText
 from nonebot.rule import to_me
 
-memo_command_group = CommandGroup("memo", prefix_aliases=True)
+memo_command_group = CommandGroup("memo", rule=to_me())
 
 help_command = memo_command_group.command("help")
 @help_command.handle()
-async def _(bot, event: MessageEvent, ars: str = ArgPlainText()):
+async def _(bot, event: MessageEvent):
     await help_command.finish(Message([
         MessageSegment.reply(event.message_id),
         MessageSegment.text("定时备忘录帮助文档：\n"),
@@ -24,25 +23,23 @@ async def _(bot, event: MessageEvent, ars: str = ArgPlainText()):
 
 add_command = memo_command_group.command("add")
 @add_command.handle()
-async def _(bot, event: MessageEvent, ars: str = ArgPlainText()):
+async def _(bot, event: MessageEvent):
     pass
 
 list_command = memo_command_group.command("list")
 @list_command.handle()
-async def _(bot, event: MessageEvent, ars: str = ArgPlainText()):
+async def _(bot, event: MessageEvent):
     pass
 
 delete_command = memo_command_group.command("delete")
 @delete_command.handle()
-async def _(bot, event: MessageEvent, ars: str = ArgPlainText()):
+async def _(bot, event: MessageEvent):
     pass
 
 edit_command = memo_command_group.command("edit")
 @edit_command.handle()
-async def _(bot, event: MessageEvent, ars: str = ArgPlainText()):
+async def _(bot, event: MessageEvent):
     pass
 
 async def memo_bot_connect_handler(bot: Bot) -> Optional[Message]:
     return Message("定时备忘录已启动喵~")
-
-__all__ = ["memo_bot_connect_handler"]
